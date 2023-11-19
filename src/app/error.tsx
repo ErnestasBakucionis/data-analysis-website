950b010aea0e5798e5fff302dc5a76936b368136
Ernestas <34349429+ErnestasBakucionis@users.noreply.github.com>
2023-11-19 18:51:25 +0200

    fixed localization of errors and loading

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,13 +1,15 @@
 "use client";
-
+import useTranslation from "@/utils/useTranslation";
 import React from "react";
 
 export default function Error() {
+    const { t } = useTranslation();
+
     return (
         <div className="flex h-screen w-full flex-col items-center justify-center gap-4 bg-white">
-            <div className="text-6xl font-bold text-red-600">Error</div>
-            <p className="text-xl font-semibold">Something Went Wrong</p>
-            <p className="text-md">Please try again later or contact support.</p>
+            <div className="text-6xl font-bold text-red-600">{t("error")}</div>
+            <p className="text-xl font-semibold">{t("errorTitle")}</p>
+            <p className="text-md">{t("errorDescription")}</p>
         </div>
     );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,15 @@
+"use client"
+import useTranslation from "@/utils/useTranslation";
 import React from "react";
 
 export default function NotFound() {
+    const { t } = useTranslation();
 
     return (
         <div className="flex h-screen w-full flex-col items-center justify-center gap-4 bg-white">
             <div className="text-9xl font-bold">404</div>
-            <p className="text-xl font-semibold">Page Not Found</p>
-            <p className="text-md">The page you are looking for does not exist.</p>
+            <p className="text-xl font-semibold">{t("pageNotFoundTitle")}</p>
+            <p className="text-md">{t("pageNotFoundDescription")}</p>
         </div>
     );
 }
